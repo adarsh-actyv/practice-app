0c3b76e45ac1fb0d2a344a80a5ade811746dab25
adarsh-actyv <adarsh.singh@actyv.com>
2022-08-18 23:24:10 +0530

    signup functionality implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  public signUp() {
-    return { message: 'Sign Up' };
+  async signUp(dto: AuthDto) {
+    try {
+      // generate password hash
+      const hash = await argon.hash(dto.password);
+
+      // save new user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: dto.firstName,
+        },
+      });
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already in use');
+        }
+      }
+      throw error;
+    }
   }
 
   public signIn() {
