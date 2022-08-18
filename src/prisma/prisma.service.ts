7a20f63811461ad3eb51400b659595bd81b1013e
adarsh-actyv <adarsh.singh@actyv.com>
2022-08-18 22:24:09 +0530

    more of controllers and services

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'http://localhost:8080/datasources',
+        },
+      },
+    });
+  }
+}
