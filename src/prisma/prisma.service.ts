ed8fe1afd0ac8474578e03c1591f56cc81429102
adarsh-actyv <adarsh.singh@actyv.com>
2022-08-19 12:59:30 +0530

    using nestjs configs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'http://localhost:8080/datasources',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
